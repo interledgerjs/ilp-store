33559f96d3abf884879251f858b56428981aaa61
adrianhopebailie <adrian@hopebailie.com>
2018-11-23 14:42:19 +0200

    fix: package structure

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,9 +1,12 @@
 import 'mocha'
-import createStore,  { isStore } from '..'
+import createStore, { isStore } from '..'
 import * as sinon from 'sinon'
 import * as Chai from 'chai'
 import * as chaiAsPromised from 'chai-as-promised'
+import * as path from 'path'
+
 Chai.use(chaiAsPromised)
+const modulePath = path.join(__dirname, '../../build/__test__/mocks/store')
 const assert = Object.assign(Chai.assert, sinon.assert)
 require('source-map-support').install()
 
@@ -25,53 +28,52 @@ describe('ilp-store', function () {
 
     it('should use ilp-store-memory if no module name is provided', function () {
       const store = createStore()
-      assert(store.constructor.name === "MemoryStore")
+      assert(store.constructor.name === 'MemoryStore')
     })
 
     it('should throw an error if the provided module is not found', function () {
-      assert.throws(() => createStore({store: 'non-existent-module'}), /Cannot find module \'non-existent-module\'/)
+      assert.throws(() => createStore({ store: 'non-existent-module' }), /Cannot find module \'non-existent-module\'/)
     })
 
     it('should throw an error if the provided module is not a store', function () {
-      assert.throws(() => createStore({store: 'crypto'}))
+      assert.throws(() => createStore({ store: 'crypto' }))
     })
 
     it('should load the named module', function () {
-      const store = createStore({store: '../test/mocks/store', options: { test: true}})
-      assert(store.constructor.name === "TestStore")
+      const store = createStore({ store: modulePath, options: { test: true } })
+      assert(store.constructor.name === 'TestStore')
     })
 
     it('should load the store with the given options', function () {
-      const store = createStore({store: '../test/mocks/store', options: { prefix: 'TEST'}})
+      const store = createStore({ store: modulePath, options: { prefix: 'TEST' } })
       assert((store as any).prefix === 'TEST')
     })
 
     it('should load the store named in env var ILP_STORE if available', function () {
-      process.env['ILP_STORE'] = '../test/mocks/store'
+      process.env['ILP_STORE'] = modulePath
       const store = createStore()
-      assert(store.constructor.name === "TestStore")
+      assert(store.constructor.name === 'TestStore')
     })
 
     it('should prefer the store named in parameters over the env var ILP_STORE', function () {
       process.env['ILP_STORE'] = 'ilp-plugin-memory'
-      const store = createStore({store: '../test/mocks/store', options: { prefix: 'TEST'}})
-      assert(store.constructor.name === "TestStore")
+      const store = createStore({ store: modulePath, options: { prefix: 'TEST' } })
+      assert(store.constructor.name === 'TestStore')
     })
 
     it('should load the options in env var ILP_STORE_OPTIONS if available', function () {
-      process.env['ILP_STORE'] = '../test/mocks/store'
+      process.env['ILP_STORE'] = modulePath
       process.env['ILP_STORE_OPTIONS'] = '{"prefix":"TEST"}'
       const store = createStore()
       assert(store.constructor.name === 'TestStore')
       assert((store as any).prefix === 'TEST')
     })
 
-
     it('should prefer the options named in parameters over the env var ILP_STORE_OPTIONS', function () {
-      process.env['ILP_STORE'] = '../test/mocks/plugin'
+      process.env['ILP_STORE'] = 'wrong-path'
       process.env['ILP_STORE_OPTIONS'] = '{"prefix":"WRONG"}'
-      const store = createStore({store: '../test/mocks/store', options: { prefix: 'TEST'}})
-      assert(store.constructor.name === "TestStore")
+      const store = createStore({ store: modulePath, options: { prefix: 'TEST' } })
+      assert(store.constructor.name === 'TestStore')
       assert((store as any).prefix === 'TEST')
     })
 
@@ -92,7 +94,7 @@ describe('ilp-store', function () {
     })
 
     it('should return true for TestStore', function () {
-      assert(isStore(createStore({store: '../test/mocks/store'})))
+      assert(isStore(createStore({ store: modulePath })))
     })
   })
 })
